eace750ab90947cfb88d21f738d84bbaa6fcce5b
Nguyễn Đạt <89953941+BeeAZ-pm-pl@users.noreply.github.com>
2022-05-17 05:08:43 +0700

    Update Main.php

--- src/AllPlayersArePoggitStaff/Main.php
+++ src/AllPlayersArePoggitStaff/Main.php
@@ -9,10 +9,18 @@ use pocketmine\event\player\PlayerJoinEvent;
 
 class Main extends PluginBase implements Listener{
 
+ private $conflict = "AntiPoggitStaffs";
+
  public function onEnable(): void{
   $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  $this->conflict();
 }
  public function onJoin(PlayerJoinEvent $ev){
   $ev->getPlayer()->setDisplayName(array_rand(["adeynes", "awzaw", "brandon15811", "cortexpe", "cthulhu", "dktapps", "intyre", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "sandertv", "shoghicp", "sof3", "matcracker", "xavier69420", "ethaniccc", "fuyutsuki", "ifera", "thedeibo", "thunder33345", "NoDanaOnlyZuul", "PEMapModder", "poggit-bot", "robske110", "PotterHarry98"]));
+}
+ public function conflict(){
+  if($this->getServer()->getPluginManager()->getPlugin($conflict) !== null){
+  $this->getServer()->getPluginManager()->disablePlugin($this);
+  $this->getLogger()->notice("AntiPoggitStaffs Plugin Conflict So Can't Work");
 }
 }
