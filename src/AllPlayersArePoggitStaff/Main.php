d3c58aab3327ac45752768ab8b32d09430c10688
Nguyễn Đạt <89953941+BeeAZ-pm-pl@users.noreply.github.com>
2022-05-16 23:23:36 +0700

    Update Main.php

--- src/AllPlayersArePoggitStaff/Main.php
+++ src/AllPlayersArePoggitStaff/Main.php
@@ -12,6 +12,6 @@ class Main extends PluginBase implements Listener=
   $this->getServer()->getPluginManager()->registerEvents($this, $this);
 }
  public function onJoin(PlayerJoinEvent $ev){
-  $ev->getPlayer()->setDisplayName(array_rand(["adeynes", "awzaw", "brandon15811", "cortexpe", "cthulhu", "dktapps", "intyre", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "sandertv", "shoghicp", "sof3", "matcracker", "xavier69420", "ethaniccc", "fuyutsuki", "ifera", "thedeibo", "thunder33345", "NoDanaOnlyZuul", "PEMapModder", "poggit-bot", "robske110", "PotterHarry98"];
+  $ev->getPlayer()->setDisplayName(array_rand(["adeynes", "awzaw", "brandon15811", "cortexpe", "cthulhu", "dktapps", "intyre", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "sandertv", "shoghicp", "sof3", "matcracker", "xavier69420", "ethaniccc", "fuyutsuki", "ifera", "thedeibo", "thunder33345", "NoDanaOnlyZuul", "PEMapModder", "poggit-bot", "robske110", "PotterHarry98"]));
 }
 }
