86892e6b63a7e6bffb6f2f37443615fa6f823cd8
ErikPDev <33035080+ErikPDev@users.noreply.github.com>
2022-05-23 18:19:06 +0300

    Get usernames from github too.

--- src/AllPlayersArePoggitStaff/Main.php
+++ src/AllPlayersArePoggitStaff/Main.php
@@ -2,24 +2,37 @@
 
 namespace AllPlayersArePoggitStaff;
 
-use pocketmine\player\Player;
-use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Internet;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener {
 
- public function onEnable(): void{
-  $this->getServer()->getPluginManager()->registerEvents($this, $this);
-  $this->conflict();
-}
- public function onJoin(PlayerJoinEvent $ev){
-  $ev->getPlayer()->setDisplayName(array_rand(["adeynes", "awzaw", "brandon15811", "cortexpe", "cthulhu", "dktapps", "intyre", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "sandertv", "shoghicp", "sof3", "matcracker", "xavier69420", "ethaniccc", "fuyutsuki", "ifera", "thedeibo", "thunder33345", "NoDanaOnlyZuul", "PEMapModder", "poggit-bot", "robske110", "PotterHarry98"]));
-}
- public function conflict(){
-  if($this->getServer()->getPluginManager()->getPlugin("AntiPoggitStaffs") !== null){
-  $this->getServer()->getPluginManager()->disablePlugin($this);
-  $this->getLogger()->notice("AntiPoggitStaffs Plugin Conflict So Can't Work");
-}
-}
+	private array $users = ["adeynes", "awzaw", "brandon15811", "cortexpe", "cthulhu", "dktapps", "intyre", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "sandertv", "shoghicp", "sof3", "matcracker", "xavier69420", "ethaniccc", "fuyutsuki", "ifera", "thedeibo", "thunder33345", "NoDanaOnlyZuul", "PEMapModder", "poggit bot", "robske110", "PotterHarry98"];
+
+	public function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->conflict();
+		$this->loadUsernames();
+	}
+
+	private function conflict() {
+		if ($this->getServer()->getPluginManager()->getPlugin("AntiPoggitStaffs") == null) return;
+		$this->getServer()->getPluginManager()->disablePlugin($this);
+		$this->getLogger()->notice("AntiPoggitStaffs Plugin Conflict So Can't Work");
+	}
+
+	private function loadUsernames() {
+		$request = Internet::getURL("https://api.github.com/orgs/poggit/members", 10, [], $err);
+		if ($err) return;
+		$accounts = json_decode($request->getBody(), true);
+		foreach ($accounts as $account) {
+			$this->users[] = $account["login"];
+		}
+	}
+
+	public function onJoin(PlayerJoinEvent $ev) {
+		$ev->getPlayer()->setDisplayName(array_rand($this->users));
+	}
 }
